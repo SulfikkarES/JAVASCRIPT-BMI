c03923f60a4a22b34293d236812ab1f8f04b1dbd
SULFIKKAR E S <sulfikkeres369@hotmail.com>
2021-12-29 19:28:31 +0530

    https://github.com/SulfikkarES

---
+++ index/bmi.js
@@ -0,0 +1,88 @@
+
+            
+            function saturation1()
+            {
+                console.log(saturation.value);
+                if(saturation.value==="")
+                {
+                    alert("Blank Saturation is not allowed ");
+                }
+                else if(saturation.value=="---Select---")
+                {
+                    alert("Blank Saturation is not allowed ");
+                }
+            }
+
+            function FindName()
+            {
+                console.log(fullName.value);
+                if(fullName.value==="")
+                {
+                    alert("Blank Full Name is not allowed ");
+                }
+            }
+
+            function initial1()
+            {
+                console.log(initial.value);
+                if(initial.value==="")
+                {
+                    alert("Blank Initial is not allowed ");
+                }
+                
+            }
+
+
+            function Findaddress()
+            {
+                console.log(address.value);
+                if(address.value==="")
+                {
+                    alert("Blank Address is not allowed ");
+                }
+            }
+
+            function FindAge()
+            {
+                console.log(age.value);
+                if(age.value==="")
+                {
+                    alert("Blank Age is not allowed ");
+                }
+            }
+
+            function FindHeight()
+            {
+                console.log(height.value);
+                if(height.value==="")
+                {
+                    alert("Blank Height is not allowed ");
+                }
+            }
+
+            function FindWeight()
+            {
+                console.log(weight.value);
+                if(weight.value==="")
+                {
+                    alert("Blank Weight is not allowed ");
+                }
+            }
+
+            function CalculateBMI()
+            {
+                console.log(bmi.value);
+                if(bmi.value==="")
+                {
+                    alert("Blank BMI is not allowed ");
+                }
+            }
+
+            function FindDescription()
+            {
+                console.log(description.value);
+                if(description.value==="")
+                {
+                    alert("Blank Description is not allowed ");
+                }
+            }
